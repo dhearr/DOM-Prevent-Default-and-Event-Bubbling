2a44e13cefee791211138e474a112422debfe932
dhearr <dhearr27@gmail.com>
2024-01-25 10:44:58 +0700

    first update

---
+++ script.js
@@ -0,0 +1,25 @@
+// =====================================================================================================
+// // PREVENT DEFAULT & EVENT BUBBLING
+// const close = document.querySelectorAll(".close");
+// close.forEach(function (element) {
+//   element.addEventListener("click", function (event) {
+//     event.target.parentElement.style.display = "none";
+//     event.preventDefault();
+//     event.stopPropagation();
+//   });
+// });
+
+// const cards = document.querySelectorAll(".card");
+// cards.forEach(function (card) {
+//   card.addEventListener("click", function () {
+//     alert("ok");
+//   });
+// });
+
+// const container = document.querySelector(".container");
+// container.addEventListener("click", function (event) {
+//   if (event.target.className == "close") {
+//     event.target.parentElement.style.display = "none";
+//   }
+// });
+// =====================================================================================================
